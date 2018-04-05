011ca415bb9fc5f3448dc73efc4727bd6da1dd50
Bushy.Netshidaulu <Bushy.Netshidaulu@03rnbc-ntpc0204.za.ds.naspers.com>
2018-04-05 14:28:50 +0200

    String builder method #append

--- StringBuilderP/src/Run.java
+++ StringBuilderP/src/Run.java
@@ -2,7 +2,11 @@
 public class Run {
 
 	public static void main(String[] args) {
-
+        StringBuilder stringBuilder =  new StringBuilder("Bushy");
+        stringBuilder.append("-");
+        stringBuilder.append("Given");
+        
+        System.out.println(stringBuilder);
 	}
 
 }
